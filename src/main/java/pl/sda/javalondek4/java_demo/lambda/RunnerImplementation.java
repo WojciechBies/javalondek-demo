db01b32dc247c3dd65b8ce0641ab2fc754a74124
Wojciech Bies <bies.wojciech@gmail.com>
2021-01-17 12:02:24 +0000

    [add] - example interface with 2 methods

--- src/main/java/pl/sda/javalondek4/java_demo/lambda/RunnerImplementation.java
+++ src/main/java/pl/sda/javalondek4/java_demo/lambda/RunnerImplementation.java
@@ -1,9 +1,15 @@
 package pl.sda.javalondek4.java_demo.lambda;
 
 public class RunnerImplementation implements Runner{
+
     @Override
     public void go() {
         System.out.println("Dawaj");
+    }
+
+    @Override
+    public void go(int speed) {
+        System.out.println("Speed is" + speed);
     }
 
     public void walk() {
