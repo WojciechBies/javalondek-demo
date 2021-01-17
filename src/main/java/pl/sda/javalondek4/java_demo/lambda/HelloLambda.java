ef6df93de4f79356c3690b906bcac1dbc5990262
Wojciech Bies <bies.wojciech@gmail.com>
2021-01-17 11:15:40 +0000

    [change] - first exercises with lambda

--- src/main/java/pl/sda/javalondek4/java_demo/lambda/HelloLambda.java
+++ src/main/java/pl/sda/javalondek4/java_demo/lambda/HelloLambda.java
@@ -7,7 +7,7 @@ public class HelloLambda {
         myRunner.walk();
 
         Runner runner = new RunnerImplementation();
-       // runner.walk(); - doesn't compile
+        // runner.walk(); - doesn't compile
 
         Runner anonymousRunner = new Runner() {
             @Override
@@ -22,10 +22,21 @@ public class HelloLambda {
                 System.out.println("runner");
             }
         });
+
+        handlerRunner(() -> System.out.println("inside lambda call"));
+
+        Runner myFirstLambda = () -> {
+        };
+        Runner mySecondLambda = () -> System.out.println("print");
+
+        mySecondLambda.go(5);
+        mySecondLambda.go();
+        Runner.walk();
     }
 
     public static void handlerRunner(Runner runner) {
         System.out.println("handleRunner()");
         runner.go();
     }
+
 }
