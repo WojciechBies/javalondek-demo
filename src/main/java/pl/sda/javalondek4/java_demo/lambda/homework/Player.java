f8709efbcd48c52831a1db7f3dbbdc9cbb072c66
Wojciech Bies <bies.wojciech@gmail.com>
2021-01-18 23:24:59 +0000

    [add] - Functional interface, class Game, class Player

---
+++ src/main/java/pl/sda/javalondek4/java_demo/lambda/homework/Player.java
@@ -0,0 +1,14 @@
+package pl.sda.javalondek4.java_demo.lambda.homework;
+
+public class Player {
+
+    String playerName;
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+}
