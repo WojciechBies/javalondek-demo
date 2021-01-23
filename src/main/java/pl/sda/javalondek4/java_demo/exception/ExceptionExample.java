1393e9ceaff7deee6cbe77c82393fd2a93c5e001
Wojciech Bies <bies.wojciech@gmail.com>
2021-01-23 09:52:18 +0000

    [add] - runtime exception example

--- src/main/java/pl/sda/javalondek4/java_demo/exception/ExceptionExample.java
+++ src/main/java/pl/sda/javalondek4/java_demo/exception/ExceptionExample.java
@@ -15,6 +15,11 @@ public class ExceptionExample {
         }
         System.out.println("My nick is: " + myNick);
 
+        System.out.println("Now with runtime exception");
+
+        String s = exg.nickWithRuntimeException();
+        System.out.println(s);
+
         String nullString = null;
         nullString.concat("ala ma kota");
     }
--- src/main/java/pl/sda/javalondek4/java_demo/exception/ExceptionGenerator.java
+++ src/main/java/pl/sda/javalondek4/java_demo/exception/ExceptionGenerator.java
@@ -1,16 +1,37 @@
 package pl.sda.javalondek4.java_demo.exception;
 
 import java.util.Random;
-
 public class ExceptionGenerator {
-
     public String nick() throws NoNickException {
-        Random random  = new Random();
+        Random random = new Random();
         if (random.nextInt(100) % 2 == 0) {
-            return "slaughter";
+            return "maniek";
+        } else {
+            throw new NoNickException("I have no nick... yet:P");
         }
-        else {
-            throw new NoNickException("I have no nick");
+    }
+    public String nickv2() {
+        String myNick;
+        try { // first step
+            myNick = nick();
+        } catch (NoNickException e) { // second - sometimes
+            System.out.println("Problem with nick generator...");
+            myNick = "this user has no nick";
+        } finally { // third // always
+            // myNick = "from finally:)"; // it always overrides nick:(
+        }
+        return myNick;
+    }
+    /**
+     * @throws NoNickException
+     * @return
+     */
+    public String nickWithRuntimeException() throws NoNickRuntimeException {
+        Random random = new Random();
+        if (random.nextInt(100) % 2 == 0) {
+            return "maniek";
+        } else {
+            throw new NoNickRuntimeException("I have no nick... yet:P");
         }
     }
 }
