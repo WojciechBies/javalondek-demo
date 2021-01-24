25d40c25260c38af3f2b7151bbc70cf49801bbfb
Wojciech Bies <bies.wojciech@gmail.com>
2021-01-24 12:08:34 +0000

    playing with Optional<> class

--- src/main/java/pl/sda/javalondek4/java_demo/optional/MySecret.java
+++ src/main/java/pl/sda/javalondek4/java_demo/optional/MySecret.java
@@ -1,21 +1,21 @@
 package pl.sda.javalondek4.java_demo.optional;
 
-import java.util.Objects;
+import java.util.Optional;
 
 import static java.util.Objects.*;
 
-public class MySecret {
-    private String secret;
+public class MySecret<T> {
+    private T secret;
 
-    public MySecret(String secret) {
+    public MySecret(T secret) {
         this.secret = secret;
     }
 
-    public String getSecret() {
+    public T getSecret() {
         return secret;
     }
 
-    public void setSecret(String secret) {
+    public void setSecret(T secret) {
         this.secret = secret;
     }
 
@@ -27,14 +27,19 @@ public class MySecret {
     }
 
     public static void main(String[] args) {
-        MySecret realOne = new MySecret("I like...");
-        MySecret withNullContent = new MySecret(null);
+        MySecret<String> realOne = new MySecret<>("I like...");
+        MySecret<String> withNullContent = new MySecret<>(null);
         MySecret.printSecretLength(realOne);
         MySecret.printSecretLength(withNullContent);
 
+        Optional<Integer> number = Optional.of(42);
+        Optional<Integer> nullNumber = Optional.empty();
+
+
+
     }
 
-    public static void printSecretLength(MySecret mySecret) {
+    public static void printSecretLength(MySecret<String> mySecret) {
 
          int secret = mySecret.getSecret() != null ? mySecret.getSecret().length() : 0;
          secret = nonNull(mySecret.getSecret()) ? mySecret.getSecret().length() : 0;
